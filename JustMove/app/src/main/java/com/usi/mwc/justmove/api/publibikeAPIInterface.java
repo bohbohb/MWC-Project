e2b6c1d48dcd6bdbf583560865e91c986080fab9
bohbohb <federico.lombardo.97@gmail.com>
2021-11-16 19:14:53 +0100

    some stuff done

--- JustMove/app/src/main/java/com/usi/mwc/justmove/api/publibikeAPIInterface.java
+++ JustMove/app/src/main/java/com/usi/mwc/justmove/api/publibikeAPIInterface.java
@@ -1,12 +1,35 @@
 package com.usi.mwc.justmove.api;
 
+import com.usi.mwc.justmove.model.Station;
 import com.usi.mwc.justmove.model.Stations;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface publibikeAPIInterface {
 
-    @GET("partner/stations/")
-    Call<Stations> getNoticeData();
+    /**
+     *
+     * @return all visible PubliBike stations including all the vehicles currently at a station.
+     */
+    @GET("partner/stations")
+    Call<Stations> getStationsData();
+
+    /**
+     *
+     * @return display the location of the stations on a map and whether or not bikes are available.
+     * (id, lat, log, state)
+     */
+    @GET("stations")
+    Call<Stations> getLocationsData();
+
+    /**
+     *
+     * @param stationId
+     * @return Returns detailed information about a specific station and the vehicels currently at this station.
+     */
+    @GET("partner/stations/{id}")
+    Call<Station> getStationData(@Path("id") int stationId);
+
 }
