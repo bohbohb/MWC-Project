7318735e0b93b9faf0c189ed9f428911c82bea60
OttavioBuonomoUSI <buonoo@usi.ch>
2021-11-16 19:18:36 +0100

    Upload MainActivity

---
+++ JustMove/app/src/main/java/com/usi/mwc/justmove/MainActivity.java
@@ -0,0 +1,43 @@
+package com.usi.mwc.justmove;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavView = findViewById(R.id.nav_view);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        Set<Integer> destinations = new HashSet<>();
+        destinations.add(R.id.travelFragment);
+        destinations.add(R.id.myTravels);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(destinations).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(bottomNavView, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        return navController.navigateUp();
+    }
+}
